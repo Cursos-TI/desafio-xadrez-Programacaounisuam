7182bd6d21ff2c521a39cc8cca61865d5df40c23
Programacaounisuam <fgomesf96@gmail.com>
2025-03-03 23:39:37 +0000

    Desafio do Xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,89 @@
 #include <stdio.h>
+//Desáfio Xadrez nível metre
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+// Recursivo da torre 
+void movertorre(int torrer){
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    if(torrer > 0){
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
+        movertorre (torrer -1);
+        printf("Direita \n");
+      
+    }
 }
+
+// Recursivo da rainha
+void moverrainha(int rainhar){
+
+    if(rainhar > 0){
+
+        moverrainha(rainhar -1);
+        printf("Esquerda \n");
+      
+    }
+}
+
+// Recursivo da bispo
+void moverbispo(int bispor ){
+
+    while (bispor > 0 )
+{
+    for ( int i = 1; i <= 1; i++)
+    {
+        printf("Direita \n");
+    }
+
+    bispor --;
+    printf("Cima \n");
+    
+}
+
+}
+
+int main () {
+
+
+// chamada do recursivo da torre 
+int torre = 5;
+
+printf("Movimento da torre \n");
+movertorre(torre);
+
+
+
+// chamada do recursivo do bispo
+
+int bispo = 5;
+printf("Movimento do bispo \n");
+moverbispo(bispo);
+
+
+
+// chamada do recursivo da rainha
+
+int rainha = 8;
+printf("Movimento da rainha \n");
+moverrainha(rainha);
+
+//Movimento do cavalo
+ 
+printf("Movimento do cavalo \n");
+
+int i = 1;
+int j = 1;
+
+while (i <= 1 )
+{
+    for ( j = 1; j <= 2; j++)
+    {
+        printf("Cima \n");
+    }
+    
+    printf("Direita \n");
+    i++;
+    
+}
+return 0;
+
+}
+
